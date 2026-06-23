7a83f137611fa3bd7ca0943f7416331adf0011a3
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-06-22 21:06:43 -0300

    Add Noudoc component exports

---
+++ src/lib/components/noudoc/index.ts
@@ -0,0 +1,10 @@
+export { default as ActionMenu } from "./action-menu.svelte";
+export { default as AppShell } from "./app-shell.svelte";
+export { default as DataToolbar } from "./data-toolbar.svelte";
+export { default as MetricCard } from "./metric-card.svelte";
+export { default as PageHeader } from "./page-header.svelte";
+export { default as SectionHeader } from "./section-header.svelte";
+export { default as StatusBadge } from "./status-badge.svelte";
+
+export type { ActionMenuItem } from "./action-menu.svelte";
+export type { AppShellNavItem } from "./app-shell.svelte";
